44d6ec1ab0ac88f9282be208c8f625a6bb4f53d5
highlordtaha <tahaguneramet@gmail.com>
2025-12-18 14:48:31 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,53 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int main(int argc, char* argv[]) {
+struct Ogrenci {
+    int dogruSayisi;
+    int yanlisSayisi;
+    int bosSayisi;
+    int net;
+};
 
+int main(int argc, char *argv[])
+{
     FILE *ogrenciDosya, *anahtarDosya;
     char ogrCevap, dogruCevap;
-    int dogru = 0, yanlis = 0, bos = 0;
+    struct Ogrenci ogr;
 
-    if (argc != 3) {
-        printf("Dogru kullanim sekli \"./main.exe [ogrencino].txt anahtar.txt\" seklindedir.\n");
+    ogr.dogruSayisi = 0;
+    ogr.yanlisSayisi = 0;
+    ogr.bosSayisi = 0;
+    ogr.net = 0;
+
+    if (argc != 3)
+    {
+        printf("Kullanim: %s ogrenci.txt anahtar.txt\n", argv[0]);
         return 1;
     }
 
     ogrenciDosya = fopen(argv[1], "r");
     anahtarDosya = fopen(argv[2], "r");
 
-    if (ogrenciDosya == NULL || anahtarDosya == NULL) {
-        printf("Dosya acilamadi.\n");
+    if (ogrenciDosya == NULL || anahtarDosya == NULL)
+    {
+        perror("Dosya acilamadi");
         return 1;
     }
 
-    while (fscanf(ogrenciDosya, " %c", &ogrCevap) != EOF && fscanf(anahtarDosya, " %c", &dogruCevap) != EOF) {
-
+    while (fscanf(ogrenciDosya, " %c", &ogrCevap) != EOF &&
+           fscanf(anahtarDosya, " %c", &dogruCevap) != EOF)
+    {
         if (ogrCevap == '-')
-            bos++;
+        {
+            ogr.bosSayisi++;
+        }
         else if (ogrCevap == dogruCevap)
-            dogru++;
+        {
+            ogr.dogruSayisi++;
+        }
         else
-            yanlis++;
+        {
+            ogr.yanlisSayisi++;
+        }
     }
 
     fclose(ogrenciDosya);
     fclose(anahtarDosya);
 
-    char ogrNo[20];
-    strcpy(ogrNo, argv[1]);
-    char* nokta = strrchr(ogrNo, '.'); // sondan '.' ara
-    if (nokta != NULL) {
-        *nokta = '\0'; // '.' karakterini string sonu yap
-    }
+    ogr.net = ogr.dogruSayisi - (ogr.yanlisSayisi / 4);
 
-    int net = dogru - (yanlis / 4);
-
-    printf("%s nolu ogrencinin\n", ogrNo);
-    printf("Dogru sayisi: %d\n", dogru);
-    printf("Yanlis sayisi: %d\n", yanlis);
-    printf("Bos sayisi: %d\n", bos);
-    printf("Net: %d\n", net);
+    printf("%s nolu ogrencinin\n", argv[1]);
+    printf("Dogru sayisi: %d\n", ogr.dogruSayisi);
+    printf("Yanlis sayisi: %d\n", ogr.yanlisSayisi);
+    printf("Bos sayisi    : %d\n", ogr.bosSayisi);
+    printf("Net           : %d\n", ogr.net);
 
     return 0;
 }
